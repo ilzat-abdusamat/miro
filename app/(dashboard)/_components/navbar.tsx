53a47b077261a7a7947cbf4024affcc70a8067ae
ilzat <ilzatabdusamat@gmail.com>
2024-03-02 17:14:19 -0500

    feat: add organization sidebar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
+import { SearchInput } from './search-input';
 
 const Navbar = () => {
   return (
     <div className='flex items-center gap-x-4 p-5'>
+      <div>
+        <SearchInput />
+      </div>
       <UserButton />
     </div>
   );
