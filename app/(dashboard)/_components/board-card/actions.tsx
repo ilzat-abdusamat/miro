5a9bc5f31748b5f2e40905eedb247c6382adb719
ilzat <ilzatabdusamat@gmail.com>
2024-03-12 17:25:35 -0400

    feat: confirm modal for delete board

--- app/(dashboard)/_components/board-card/actions.tsx
+++ app/(dashboard)/_components/board-card/actions.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { ConfirmModal } from '@/components/confirm-modal';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,14 +11,19 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { api } from '@/convex/_generated/api';
 import { useApiMutation } from '@/hooks/use-api-mutation';
+import { DropdownMenuContentProps } from '@radix-ui/react-dropdown-menu';
+import { Link2Icon, Trash2Icon } from 'lucide-react';
 import { toast } from 'sonner';
 
 interface ActionsProps {
   children: React.ReactNode;
+  side?: DropdownMenuContentProps['side'];
+  sideOffset?: DropdownMenuContentProps['sideOffset'];
   id: string;
+  title: string;
 }
 
-const Actions = ({ children, id }: ActionsProps) => {
+const Actions = ({ children, side, sideOffset, id, title }: ActionsProps) => {
   const { mutate: deleteBoard, pending } = useApiMutation(api.board.deleteBoard);
 
   const onDelete = () => {
@@ -25,19 +32,46 @@ const Actions = ({ children, id }: ActionsProps) => {
       .catch(() => toast.error('Failed to delete board'));
   };
 
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success('Link copied'))
+      .catch(() => toast.error('Failed to copy link'));
+  };
+
   return (
     <DropdownMenu>
       <div className='absolute z-50 top-1 right-1'>
         <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-        <DropdownMenuContent onClick={(e) => e.stopPropagation()}>
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuContent
+          onClick={(e) => e.stopPropagation()}
+          side={side}
+          sideOffset={sideOffset}
+          className='w-60'
+        >
           <DropdownMenuItem>Rename</DropdownMenuItem>
           <DropdownMenuItem
-            onClick={onDelete}
+            onClick={onCopyLink}
+            className='p-3 cursor-pointer'
+          >
+            <Link2Icon className='h-4 w-4 mr-2' />
+            Copy board link
+          </DropdownMenuItem>
+
+          <ConfirmModal
+            onConfirm={onDelete}
+            header='Delete board?'
+            description='This will delete the board and all of its contents.'
             disabled={pending}
           >
-            Delete
-          </DropdownMenuItem>
+            <Button
+              variant='ghost'
+              className='p-3 cursor-pointer text-sm w-full justify-start font-normal'
+            >
+              <Trash2Icon className='h-4 w-4 mr-2' />
+              Delete
+            </Button>
+          </ConfirmModal>
         </DropdownMenuContent>
       </div>
     </DropdownMenu>
--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -43,7 +43,11 @@ export const BoardCard = (props: BoardCardProps) => {
             className='object-fit'
           />
           <Overlay />
-          <Actions id={props.id}>
+          <Actions
+            id={props.id}
+            title={props.title}
+            side='right'
+          >
             <button className='absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none'>
               <MoreHorizontalIcon className='text-white opacity-75 hover:opacity-100 transition-opacity' />
             </button>
